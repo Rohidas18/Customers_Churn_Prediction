5cf250dfd144ff038723e3fbc660c4d1fb9892e8
Rohidas <patilrohidas1801@gmail.com>
2026-02-21 20:09:32 +0530

    Churn_prediction

---
+++ SQL_queries/SQLQuery6.sql
@@ -0,0 +1,15 @@
+
+--Create View for Power BI
+
+
+CREATE VIEW vw_ChurnData AS
+SELECT *
+FROM prod_Churn
+WHERE Customer_Status IN ('Churned', 'Stayed');
+GO
+
+CREATE VIEW vw_JoinData AS
+SELECT *
+FROM prod_Churn
+WHERE Customer_Status = 'Joined';
+GO
